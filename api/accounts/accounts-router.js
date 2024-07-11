035c1042c81084b398ba0b2940f1aeef9607d028
DerekG29 <derekgrubbs29@gmail.com>
2024-07-10 20:03:30 -0400

    built DELETE endpoint for /:id

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -5,6 +5,7 @@ const {
   getById,
   create,
   updateById,
+  deleteById
 } = require('./accounts-model');
 
 router.get('/', async (req, res, next) => {
@@ -12,7 +13,7 @@ router.get('/', async (req, res, next) => {
     const accounts = await getAll();
     res.json(accounts);
   } catch (error) {
-    next({ message: 'Cannot GET accounts...' });
+    next(error);
   }
 });
 
@@ -22,7 +23,7 @@ router.get('/:id', async (req, res, next) => {
     const account = await getById(id);
     res.json(account);
   } catch (error) {
-    next({ message: `Cannot GET account with id ${id}...` });
+    next(error);
   }
 });
 
@@ -32,7 +33,7 @@ router.post('/', async (req, res, next) => {
     const newAccount = await getById(id);
     res.status(201).json(newAccount);
   } catch (error) {
-    next({ message: 'Cannot POST new account...' });
+    next(error);
   }
 })
 
@@ -43,17 +44,23 @@ router.put('/:id', async (req, res, next) => {
     const updated = await getById(id);
     res.json(updated);
   } catch (error) {
-    next({ message: `Cannot PUT updates to account with id ${id}...` });
+    next(error);
   }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await deleteById(id);
+    res.json(req.account);
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-  res.status(500).json(err);
+router.use((error, req, res, next) => { // eslint-disable-line
+  console.error(error);
+  res.status(500).json({ message: `${req.method} to /api/accounts${req.url} failed...` });
 })
 
 module.exports = router;
